41fe8f3c209b5172ee9dd3e6aeeae3638022fa38
Robert Smith <rbrtsmith84@googlemail.com>
2015-10-27 21:50:10 +0100

    setup done

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,19 @@ var gulp = require('gulp'),
     autoprefixer = require('gulp-autoprefixer'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
-    livereload = require('gulp-livereload');
+    livereload = require('gulp-livereload'),
+    header = require('gulp-header'),
+    pkg = require('./package.json'),
+    banner = ['/**',
+      ' * <%= pkg.name %> - <%= pkg.description %>',
+      ' * @version v<%= pkg.version %>',
+      ' * @link <%= pkg.homepage %>',
+      ' * @license <%= pkg.license %>',
+      ' */',
+      ''].join('\n');
 
 gulp.task('style', function() {
-    return gulp.src('src/scss/style.scss')
+    return gulp.src('src/scss/rwsmith-grids.scss')
         .pipe(sourcemaps.init())
             .pipe(sass().on('error', sass.logError))
             .pipe(autoprefixer({
@@ -16,12 +25,20 @@ gulp.task('style', function() {
             }))
             .pipe(minifyCSS())
             .pipe(rename({suffix: '.min'}))
+            .pipe(header(banner, { pkg : pkg } ))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('dist'))
         .pipe(livereload());
 });
 
-gulp.task('default', ['style'], function() {
+gulp.task('html', function() {
+    return gulp.src('src/index.html')
+        .pipe(gulp.dest('dist'))
+        .pipe(livereload());
+});
+
+gulp.task('default', ['style', 'html'], function() {
     livereload.listen();
     gulp.watch('src/scss/**/*.scss', ['style']);
+    gulp.watch('src/index.html', ['html']);
 });
