67eb0fc19a2650b5642134bc59442c9167e1c2c5
Paul Hammond <paul.hammond@bbc.co.uk>
2016-10-30 01:23:30 +0100

    update command name

--- setup/commands/init.js
+++ setup/commands/init.js
@@ -2,5 +2,5 @@ const fs = require('fs-extra');
 
 module.exports = (program, rootDir) => {
   const destinationDirectory = program.dirname || 'scss';
-  fs.copy(__dirname + '/../resources', `${rootDir}/${destinationDirectory}`);
+  fs.copy(`${__dirname}/../resources`, `${rootDir}/${destinationDirectory}`);
 };
